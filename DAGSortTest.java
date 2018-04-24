db0b18f24d0dd6a8bba3f1d465eb25903fe7e02b
Matthew Hutchings <mwh1g17@soton.ac.uk>
2018-04-24 13:52:39 +0100

    Inital Commit

---
+++ DAGSortTest.java
@@ -0,0 +1,45 @@
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class DAGSortTest {
+	
+	@Before
+	public void setUp() {
+		
+		
+	}
+	
+	@After
+	public void tearDown() {
+		
+		
+		
+	}
+	
+	@Test
+	public void testing() {
+		
+		int test1 = 2;
+		int test2 = 4;
+		
+		assertEquals(test1,test2);
+		
+	}
+	
+	@Test
+	public void test1() {
+		
+		
+		
+		
+		
+	}
+	
+	
+
+}
